9e1ebf8af8458b60592b352de035c8edf3c90c66
Ryan Cowie <ryancowie@MacBook-Pro.local>
2019-01-16 22:51:47 -0800

    -m completed basic requirements for react-components

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-// TODO
+
+// var Cucumbers = () => (
+//   <li> cucumber </li>
+// );
+
+// var Kale = () => (
+//   <li> kale </li>
+// );  
+
+// var GroceryList = (props) => (
+//   <ul>
+//     <li> {props.grocery[0]} </li>
+//     <li> {props.grocery[1]} </li>
+//   </ul>
+// );
+ 
+
+class GroceryListItem extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            done: false
+        };
+    }
+    onListMouseOver() {
+        this.setState({
+            done: !this.state.done
+        });
+    }
+    render() {
+        var style = {
+            fontWeight: this.state.done ? 'bold' : 'normal'
+        };
+        return (
+            <li style ={style} onMouseOver={this.onListMouseOver.bind(this)}>{this.props.grocery}</li>
+        );
+    }
+}
+
+var GroceryList = (props) => (
+    <ul> 
+      {props.groceries.map(grocery =>
+        <GroceryListItem grocery={grocery}/>
+        )}
+    </ul>
+  );
+
+ReactDOM.render(<GroceryList groceries={['cumcumber', 'kale']}/>, document.getElementById('app'));
+
+
